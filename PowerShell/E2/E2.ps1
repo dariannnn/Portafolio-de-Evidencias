42e5a89b5f7be1f17e3bf6083ee6bf4d9e910a69
dariannnn <90728594+dariannnn@users.noreply.github.com>
2021-10-12 01:49:44 -0500

    Add files via upload

---
+++ PowerShell/E2/E2.ps1
@@ -0,0 +1,47 @@
+﻿#Darian Michelle Orona Aguilar
+
+
+do{
+$opc = Read-Host -Prompt "Escoja una opción 
+[1]Ver el estatus de un perfil específico en el Firewall 
+[2]Cambiar el estatus de los perfiles 
+[3]Ver el perfil de su red
+[4]Cambiar su red a otro tipo de perfil
+[5]Ver las reglas de bloqueo
+[6]Agregar regla de bloqueo de entrada para un puerto
+[7]Eliminar regla de bloqueo
+[8]Salir"
+switch($opc){
+1 { 
+Ver-StatusPerfil
+}
+2{
+Cambiar-StatusPerfil
+}
+
+3{
+Ver-PerfilRedActual
+}
+4{
+Cambiar-PerfilRedActual
+}
+5{
+Ver-ReglasBloqueo
+}
+6{
+Agregar-ReglasBloqueo
+}
+7{
+Eliminar-ReglasBloqueo
+}
+8{
+Write-Host '¡Adiós!' 
+return
+}
+default{
+Write-Host 'Opción inválida'
+}
+}
+pause
+}
+until ($opc -eq 8)
